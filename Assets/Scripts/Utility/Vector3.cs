19f8ca7a07d0e67be9fc71353d60bdc09734beea
PolPlasher <pollleida13@gmail.com>
2025-10-23 12:00:50 +0200

    [VECTOR3] Changed Vector2 to Vector3

---
+++ Assets/Scripts/Utility/Vector3.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Utility
+{
+    public struct Vector3
+    {
+        public float x, y, z;
+
+        public Vector3(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public static Vector3 operator +(Vector3 a, Vector3 b) => new(a.x + b.x, a.y + b.y, a.z + b.z);
+        public static Vector3 operator -(Vector3 a, Vector3 b) => new(a.x - b.x, a.y - b.y, a.z - b.z);
+        public static Vector3 operator -(Vector3 a) => new(0f - a.x, 0f - a.y, 0f - a.z);
+        public static Vector3 operator *(Vector3 a, float d) => new(a.x * d, a.y * d, a.z * d);
+        public static Vector3 operator *(float d, Vector3 a) => new(a.x * d, a.y * d, a.z * d);
+        public static Vector3 operator /(Vector3 a, float d) => new(a.x / d, a.y / d, a.z / d);
+
+        public static bool operator ==(Vector3 lhs, Vector3 rhs)
+        {
+            float num = lhs.x - rhs.x;
+            float num2 = lhs.y - rhs.y;
+            float num3 = lhs.z - rhs.z;
+            float num4 = num * num + num2 * num2 + num3 * num3;
+            return num4 < 9.99999944E-11f;
+        }
+        public static bool operator !=(Vector3 lhs, Vector3 rhs) => !(lhs == rhs);
+    }
+}
