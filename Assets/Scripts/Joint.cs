a07294f75a0ed2c7b5c6d7971b2d953c5e1f07cf
PolPlasher <pollleida13@gmail.com>
2025-10-24 13:19:48 +0200

    Forward kinematics stuff

---
+++ Assets/Scripts/Joint.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class Joint : MonoBehaviour
+{
+    [field: SerializeField] public float RotationSpeed { get; set; } = 30f;
+    public Vector3 DistanceToNextJoint { get; private set; }
+
+    private LineRenderer lineRenderer;
+    private Joint nextJoint;
+    [SerializeField] private float angleOffset;
+
+    private void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.startWidth = 0.1f;
+        lineRenderer.endWidth = 0.1f;
+        lineRenderer.positionCount = 2;
+        lineRenderer.startColor = Color.white;
+        lineRenderer.endColor = Color.white;
+    }
+
+    public void Initialize(Joint p_nextJoint)
+    {
+        if (!p_nextJoint) return;
+
+        DistanceToNextJoint = p_nextJoint.transform.position - transform.position;
+
+        nextJoint = p_nextJoint;
+        lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.SetPosition(1, p_nextJoint.transform.position);
+    }
+
+    private void Update()
+    {
+        lineRenderer.SetPosition(0, transform.position);
+
+        if (!nextJoint) return;
+        lineRenderer.SetPosition(1, nextJoint.transform.position);
+    }
+}
--- Assets/Scripts/MyRobotController.cs
+++ Assets/Scripts/MyRobotController.cs
@@ -1,117 +1,26 @@
 using UnityEngine;
-//using Quaternion = Utility.Quaternion;
-//using Vector3 = Utility.Vector3;
 
-public class MyRobotController : MonoBehaviour
+public class ForwardKinematics2D : MonoBehaviour
 {
-    [SerializeField] private Transform Joint1;
-    [SerializeField] private Transform Joint2;
-    [SerializeField] private Transform Joint3;
-    [SerializeField] private Transform endFactor;
-    [SerializeField] private Transform target;
-    [SerializeField] private float alpha = 0.1f;
+    [SerializeField] private Joint joint1;
+    [SerializeField] private Joint joint2;
+    [SerializeField] private Joint joint3;
 
-    [SerializeField] private float tolerance = 1f;
-    private float costFunction;
-    private Vector3 gradient;
-    private Vector3 theta;
-
-    private float l1;
-    private float l2;
-    private float l3;
-
-    void Start()
+    private void Start()
     {
-        l1 = Vector3.Distance(Joint1.position, Joint2.position);
-        l2 = Vector3.Distance(Joint2.position, Joint3.position);
-        l3 = Vector3.Distance(Joint3.position, endFactor.position);
+        joint1.Initialize(joint2);
+        joint2.Initialize(joint3);
 
-        costFunction = Vector3.Distance(endFactor.position, target.position) * Vector3.Distance(endFactor.position, target.position);
-        theta = Vector3.zero;
+        joint1.transform.LookAt(joint2.transform);
+        joint2.transform.LookAt(joint3.transform);
     }
 
     void Update()
     {
-        if (costFunction > tolerance)
-        {
-            gradient = CalculateGradient();
-            theta += -alpha * gradient;
-            endFactor.position = GetEndFactorPosition();
+        //joint1.transform.Rotate(Vector3.left, joint1.RotationSpeed * Time.deltaTime * Input.GetAxis("Vertical"));
 
+        //joint2.transform.position = joint1.transform.position + joint1.transform.rotation * joint1.DistanceToNextJoint;
 
-            Joint2.position = GetJoint1Position();
-            Joint3.position = GetJoint2Position();
-        }
-
-        costFunction = Vector3.Distance(endFactor.position, target.position) * Vector3.Distance(endFactor.position, target.position);
-    }
-
-    Vector3 CalculateGradient()
-    {
-
-        Vector3 gradientVector;
-
-        Vector3 coeff = 2 * (endFactor.position - target.position);
-
-        gradientVector.x = -coeff.x * (l1 * Mathf.Sin(theta.x)
-            + l2 * Mathf.Sin(theta.x + theta.y)
-            + l3 * Mathf.Sin(theta.x + theta.y + theta.z))
-            + coeff.y * (l1 * Mathf.Cos(theta.x) +
-            l2 * Mathf.Cos(theta.x + theta.y) +
-            l3 * Mathf.Cos(theta.x + theta.y + theta.z));
-
-        gradientVector.y = -coeff.x * (l2 * Mathf.Sin(theta.x + theta.y)
-            + l3 * Mathf.Sin(theta.x + theta.y + theta.z))
-            + coeff.y * (l2 * Mathf.Cos(theta.x + theta.y) +
-            l3 * Mathf.Cos(theta.x + theta.y + theta.z));
-
-        gradientVector.z = -coeff.x * (l3 * Mathf.Sin(theta.x + theta.y + theta.z))
-                         + coeff.y * (l3 * Mathf.Cos(theta.x + theta.y + theta.z));
-
-        gradientVector.Normalize();
-
-        return gradientVector;
-    }
-
-    Vector3 GetEndFactorPosition()
-    {
-        Vector3 newPosition;
-
-        newPosition.x = Joint1.position.x + l1 * Mathf.Cos(theta.x)
-                       + l2 * Mathf.Cos(theta.x + theta.y)
-                       + l3 * Mathf.Cos(theta.x + theta.y + theta.z);
-        newPosition.y = Joint1.position.y + l1 * Mathf.Sin(theta.x)
-                       + l2 * Mathf.Sin(theta.x + theta.y)
-                       + l3 * Mathf.Sin(theta.x + theta.y + theta.z);
-
-        newPosition.z = 0;
-
-        return newPosition;
-    }
-
-    Vector3 GetJoint2Position()
-    {
-        Vector3 newPosition;
-
-        newPosition.x = Joint1.position.x + l1 * Mathf.Cos(theta.x)
-                       + l2 * Mathf.Cos(theta.x + theta.y);
-        newPosition.y = Joint1.position.y + l1 * Mathf.Sin(theta.x)
-                       + l2 * Mathf.Sin(theta.x + theta.y);
-
-        newPosition.z = 0;
-
-        return newPosition;
-    }
-
-    Vector3 GetJoint1Position()
-    {
-        Vector3 newPosition;
-
-        newPosition.x = Joint1.position.x + l1 * Mathf.Cos(theta.x);
-        newPosition.y = Joint1.position.y + l1 * Mathf.Sin(theta.x);
-
-        newPosition.z = 0;
-
-        return newPosition;
+        //joint2.transform.Rotate(Vector3.left, joint2.RotationSpeed * Time.deltaTime * Input.GetAxis("Horizontal"));
     }
 }
