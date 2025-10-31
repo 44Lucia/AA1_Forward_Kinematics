998ac081a4de59c4ee3fc80e990b6de37aacef6c
PolPlasher <pollleida13@gmail.com>
2025-10-31 12:32:20 +0100

    [UTILITY] Added casts to Vector3 and Quaternion

--- Assets/Scripts/Utility/Quaternion.cs
+++ Assets/Scripts/Utility/Quaternion.cs
@@ -1,43 +1,59 @@
+using System;
+
 namespace Utility
 {
     public struct Quaternion
     {
+        // Fields
         public float x, y, z, w;
 
-        public Quaternion(float x, float y, float z, float w)
-        { this.x = x; this.y = y; this.z = z; this.w = w; }
+        // Constructor
+        public Quaternion(float x, float y, float z, float w) { this.x = x; this.y = y; this.z = z; this.w = w; }
 
-        public static Quaternion Identity => new(0,0,0,1);
+        // Predefined Quaternions
+        public static Quaternion Identity => new(0, 0, 0, 1);
+
+        #region Operators
+
+        // Arithmetic operators
 
         // Hamilton (a * b)
         public static Quaternion operator *(Quaternion a, Quaternion b)
         {
             return new Quaternion(
-                a.w*b.x + a.x*b.w + a.y*b.z - a.z*b.y,
-                a.w*b.y - a.x*b.z + a.y*b.w + a.z*b.x,
-                a.w*b.z + a.x*b.y - a.y*b.x + a.z*b.w,
-                a.w*b.w - a.x*b.x - a.y*b.y - a.z*b.z
+                a.w * b.x + a.x * b.w + a.y * b.z - a.z * b.y,
+                a.w * b.y - a.x * b.z + a.y * b.w + a.z * b.x,
+                a.w * b.z + a.x * b.y - a.y * b.x + a.z * b.w,
+                a.w * b.w - a.x * b.x - a.y * b.y - a.z * b.z
             );
         }
+
+        // Cast operators
+        public static explicit operator Quaternion(UnityEngine.Quaternion v) => new(v.x, v.y, v.z, v.w);
+        public static explicit operator UnityEngine.Quaternion(Quaternion v) => new UnityEngine.Quaternion(v.x, v.y, v.z, v.w);
+
+        #endregion
+
+        #region Functions
 
         public Quaternion Conjugate() => new(-x, -y, -z, w);
 
         public Quaternion Normalized()
         {
-            float n = (float)System.Math.Sqrt(x*x + y*y + z*z + w*w);
+            float n = (float)System.Math.Sqrt(x * x + y * y + z * z + w * w);
             if (n < 1e-8f) return Identity;
-            return new Quaternion(x/n, y/n, z/n, w/n);
+            return new Quaternion(x / n, y / n, z / n, w / n);
         }
 
         // q * (0,v) * q*
-        public Utility.Vector3 Rotate(Utility.Vector3 v)
+        public Vector3 Rotate(Vector3 v)
         {
             var qv = new Quaternion(v.x, v.y, v.z, 0f);
             var res = this * qv * this.Conjugate();
-            return new Utility.Vector3(res.x, res.y, res.z);
+            return new Vector3(res.x, res.y, res.z);
         }
 
-        public static Quaternion AxisAngle(Utility.Vector3 axis, float angleRad)
+        public static Quaternion AxisAngle(Vector3 axis, float angleRad)
         {
             float m = axis.Magnitude();
             if (m < 1e-8f) return Identity;
@@ -45,7 +61,9 @@ namespace Utility
             float half = angleRad * 0.5f;
             float s = (float)System.Math.Sin(half);
             float c = (float)System.Math.Cos(half);
-            return new Quaternion(nx*s, ny*s, nz*s, c);
+            return new Quaternion(nx * s, ny * s, nz * s, c);
         }
+
+        #endregion
     }
 }
